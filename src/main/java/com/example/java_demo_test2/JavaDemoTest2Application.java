703b1c3046e2ead4ed271071000d0713939efe0a
Jean <jean102020023@gmail.com>
2023-04-15 21:59:10 +0800

    add file

---
+++ src/main/java/com/example/java_demo_test2/JavaDemoTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.java_demo_test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_demo_test2/JavaDemoTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_demo_test2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
